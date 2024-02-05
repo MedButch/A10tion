e9e7b768f2f955a051e3da90fb8631afe42c74d6
MedButch <medebouchard@gmail.com>
2024-02-05 16:26:58 -0500

    Read messages from Judge

--- main.py
+++ main.py
@@ -1,43 +1,85 @@
-logger = open("log.txt", "w")
+#!/usr/bin/python3
 
-hand = []
+import sys
+
+class Card:
+    def __init__(self, name) -> None:
+        self.suit = name[0]
+        self.value = name[1]
+        match self.value:
+            case "5":
+                self.points = 5
+            case "T" | "A":
+                self.points = 10
+            case _:
+                self.points = 0
+
+    def stringify(self):
+        return f"{self.suit}{self.value}"
+    
+    def compare(self, card):
+        return card == self.stringify()
+
+class Hand:
+    def __init__(self, cards) -> None:
+        self.cards = cards
+
+    def stringify_hand(self):
+        string = ""
+        for card in self.cards:
+            string += f"{card.stringify()} "
+        return string
+
+    def remove(self, played):
+        for card in self.cards:
+            if card.compare(played):
+                self.cards.remove(card)
+                return
+
+
+hand = None
 turn = 0
 player = ""
 teammate = 0
 trick = []
 played = []
 bids = [0, 0, 0, 0]
-while (line := input()) != "end":
-    logger.write(line + "\n")
+bidWinner = 0
 
-    if line[:6] == "player":
-        player = line[7]
-        teammate = int(player) + 2 % 4
-        # logger.write(f"player number {player}, teammate {teammate}\n")
+while (line := input().split())[0] != "end":
+    print(line, file=sys.stderr)
 
-    if line[:4] == "hand":
-        hand = line[5:].split()
-        # logger.write(f"hand: {hand}\n")
+    match line[0]:
+        case "player":
+            player = line[1]
+            teammate = int(player) + 2 % 4
+            print(f"player number {player}, teammate {teammate}\n", file=sys.stderr)
 
-    if line[:3] == "bid":
-        id = line[4]
-        if id == "?":
-            print(0)
-        else:
-            bids[int(id)] = int(line[6:])
-            # logger.write(f"bids: {bids}\n")
+        case "hand":
+            hand = Hand([Card(card) for card in line[1:]])
+            print(f"hand: {hand.stringify_hand()}\n", file=sys.stderr)
+
+        case "bid":
+            if line[1] == "?":
+                print(0)
+            else:
+                bid = int(line[2])
+                bids[int(line[1])] = bid
+                if bid:
+                    bidWinner = line[1]
+
+        case "card":
+            if line[1] == "?":
+                print(0)
+            else:
+                turn = (turn + 1) % 4
+                card = line[2]
+                trick.append(card)
+                print(f"trick: {trick}\n", file=sys.stderr)
+                if not turn: #turn == 0
+                    played.extend(trick)
+                    trick = []
+                if line[1] == player:
+                    hand.remove(card)
+                    print(f"hand: {hand.stringify_hand()}\n", file=sys.stderr)
     
-    if line[:4] == "card":
-        id = line[5]
-        if id == "?":
-            print(0)
-        elif id == player:
-            hand.remove(line[7:9])
-            # logger.write(f"hand: {hand}\n")
-        else:
-            turn = (turn + 1) % 4
-            trick.append(line[7:9])
-            # logger.write(f"trick: {trick}\n")
-            if not turn: #turn == 0
-                played.extend(trick)
-                trick = []
